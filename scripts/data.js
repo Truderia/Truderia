0b8cde7ad138eb4b2353876fda018d06d116c1be
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2021-11-03 17:10:21 -0300

    Combinação de quinta

--- scripts/data.js
+++ scripts/data.js
@@ -316,17 +316,17 @@ const promotion = {
     items: [
             {
                 value:"COMBINAÇÃO DA QUINTA",
-                flavour: "NUTELLA + AMENDOIM + MORANGO",
-                description: "COLOCAR DESCRIÇÃO",
+                flavour: "DOCE DE LEITE + NOZES + BEM CASADO",
+                description: "Delicioso Trudel de Doce de Leite com adicional de Nozes e sorvete Bem Casado por um preço especial!",
                 image: "./assets/Menu/Promotion/thursdayCombination.webp",
-                price: 16,
+                price: 15,
             },
             {
                 value:"MINI-COMBINAÇÃO DA QUINTA",
                 flavour: "MINI-NUTELLA + MINI-AMENDOIM + MINI-MORANGO",
-                description: "COLOCAR DESCRIÇÃO",
+                description: "Delicioso Mini-Trudel de Doce de Leite com adicional Mini de Nozes e sorvete Bem Casado por um preço especial!",
                 image: "./assets/Menu/Promotion/thursdayCombination.webp",
-                price: 11.5,
+                price: 11,
             },
         // {
         //     value:"MINI PROMOCAO",
@@ -377,7 +377,7 @@ const promotion = {
     ]
 }
 
-// menuView.unshift(promotion)
+menuView.unshift(promotion)
 
 const entregas = {
     name: "ENTREGAS",
