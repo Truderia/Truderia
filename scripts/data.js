5b3c6ed75e04f6b145744cf7269f5c2650448c32
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2021-11-07 15:13:21 -0300

    Fix: foto potelakaroeo path

--- scripts/data.js
+++ scripts/data.js
@@ -68,7 +68,7 @@ const trudel = [
         flavour: "LAKAOREO",
         span: "",
         description: 'A junção do chocolate branco mais famoso do Brasil e o biscoito que toda família adora. Muita gostosura em apenas um recheio.',
-        image: "./assets/Menu/Trudels/Lakaoreo.webp",
+        image: "./assets/Menu/Trudels/PoteLakaOreo.webp",
         price: 15,
         miniPrice: 11
     },
