149d52409bab956c2e4ed2851c36e643a8d62d59
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2022-05-05 07:46:42 -0300

    Fix

--- scripts/data.js
+++ scripts/data.js
@@ -530,16 +530,16 @@ const promotion = {
         {
             value: "COMBINAÇÃO DA QUINTA",
             flavour: "TAMANHO NORMAL",
-            description: "Delicioso Trudel de Nutella com adicional de Amendoim e sorvete Ninho Trufado por um preço especial!",
+            description: "Delicioso Trudel de Chocolate Meio Amargo com adicional de Coco Ralado e sorvete Bem Casado por um preço especial!",
             image: "./assets/Menu/Promotion/thursdayCombination.webp",
-            price: 18.00,
+            price: 15.00,
         },
         {
             value: "MINI-COMBINAÇÃO DA QUINTA",
             flavour: "TAMANHO MINI",
-            description: "Delicioso Trudel de Nutella com adicional de Amendoim e sorvete Ninho Trufado por um preço especial!",
+            description: "Delicioso Trudel de Chocolate Meio Amargo com adicional de Coco Ralado e sorvete Bem Casado por um preço especial!",
             image: "./assets/Menu/Promotion/thursdayCombination.webp",
-            price: 14.50,
+            price: 10.50,
         },
 
     ]
@@ -552,9 +552,9 @@ const combo = {
         {
             value: "COMBO DA SEMANA",
             flavour: "Combo",
-            description: "Trudel salgado de Frango  + Mini Trudel de Creme de Ninho + Refrigerante!",
+            description: "Trudel salgado de Frango com Cheddar + Mini Trudel de KitKat + Refrigerante!",
             image: "./assets/Menu/Promotion/weekCombination.webp",
-            price: 21.00,
+            price: 23.00,
         },
     ]
 }
